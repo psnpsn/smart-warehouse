51a4f9806bb2006fc430abcc3b3be70296b6c505
psnpsn <mohamed.jridi.1@esprit.tn>
2019-12-06 23:29:37 +0100

    fixing cloud config paths; ontest

--- MSEmployees/src/main/java/tn/esprit/MSEmployees/MsEmployeesApplication.java
+++ MSEmployees/src/main/java/tn/esprit/MSEmployees/MsEmployeesApplication.java
@@ -2,8 +2,10 @@ package tn.esprit.MSEmployees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsEmployeesApplication {
 
 	public static void main(String[] args) {
