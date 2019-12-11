c12571fd46cc53bb2fcdd0795252d450f4650ef1
OussamaHAMIDI <OussamaHAMIDI@users.noreply.github.com>
2019-12-11 23:44:58 +0100

    :b: adge

--- MSTagReader/src/main/java/tn/esprit/MSTagReader/Badge.java
+++ MSTagReader/src/main/java/tn/esprit/MSTagReader/Badge.java
@@ -10,5 +10,45 @@ package tn.esprit.MSTagReader;
  * @author oussa
  */
 public class Badge {
+    private String id;
+    private String doorId;
+    private String discoveryTime;
+
+    public Badge() {
+    }
+
+    public Badge(String id, String doorId, String discoveryTime) {
+        this.id = id;
+        this.doorId = doorId;
+        this.discoveryTime = discoveryTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDoorId() {
+        return doorId;
+    }
+
+    public void setDoorId(String doorId) {
+        this.doorId = doorId;
+    }
+
+    public String getDiscoveryTime() {
+        return discoveryTime;
+    }
+
+    public void setDiscoveryTime(String discoveryTime) {
+        this.discoveryTime = discoveryTime;
+    }
+    
+    
+    
+    
     
 }
